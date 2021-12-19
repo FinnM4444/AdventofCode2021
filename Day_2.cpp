03580ad2cf48030c2fad8de35b8791c0fe726387
Finn McDonnell <fmcd743@UoA.auckland.ac.nz>
2021-12-19 23:27:13 +1300

    Fixing day 2

--- Day_2.cpp
+++ Day_2.cpp
@@ -10,8 +10,8 @@ int SumOne = 0;
 int SumTwo = 0;
 
 ifstream Depths;
-int line_number=0;
-Depths.open("C:\\Users\\finnm\\OneDrive\\Documents\\Visual Studio\\AdventofCode2021\\Day_1_Input_Values.txt");
+int line_number = 0;
+Depths.open("C:\\Users\\finnm\\OneDrive\\Documents\\Visual Studio\\AdventofCode2021\\Test_Input_File.txt");
 
     if (!Depths) {
         cout << "Unable to open file";
@@ -19,9 +19,13 @@ Depths.open("C:\\Users\\finnm\\OneDrive\\Documents\\Visual Studio\\AdventofCode2
     }
     while (Depths >> x) {
 
-        if (line_number = 3){
+        if (line_number == 3){
             if (SumTwo>SumOne){
                 increased++;
+                line_number = 0;
+                SumOne = SumTwo;
+                SumTwo = 0;
+            } else {
                 line_number = 0;
                 SumOne = SumTwo;
                 SumTwo = 0;
@@ -29,6 +33,7 @@ Depths.open("C:\\Users\\finnm\\OneDrive\\Documents\\Visual Studio\\AdventofCode2
         } else {
             SumTwo += x;
             line_number++;
+            cout << SumTwo << endl;
         }
   
     }
