ca62585895286a8bb4118a4489efa0d73e9a2056
Finn McDonnell <fmcd743@UoA.auckland.ac.nz>
2021-12-24 01:45:44 +1300

    Part way through day 3

--- Day_3.cpp
+++ Day_3.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
 
 int main() {
@@ -8,20 +9,20 @@ int main() {
 int LineCounter = 0;
 ifstream TestFileInput ("C:\\Users\\finnm\\OneDrive\\Documents\\Visual Studio\\AdventofCode2021\\Day_3_Input_Values.txt");
 string Line;
+int Length = 0;
+vector<int> DiagBits = {};
 
 
 while (getline(TestFileInput, Line)){
     int LineLength = Line.length();
-
-
-    for(int i=0; i<LineLength; i++){
-        
+    if (LineLength>Length){
+        DiagBits.resize(LineLength-Length);
     }
 
+    for(int i=0; i<LineLength; i++){
+        if (Line[i] & 1)
+    }
+    LineCounter++;
 }
-
-
-
-
 return 0;
 }
