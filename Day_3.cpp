f0d34bccc1858c07e713847323391543594f2921
Finn McDonnell <fmcd743@UoA.auckland.ac.nz>
2021-12-23 17:00:52 +1300

    Part way through day 3

---
+++ Day_3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+
+int LineCounter = 0;
+ifstream TestFileInput ("C:\\Users\\finnm\\OneDrive\\Documents\\Visual Studio\\AdventofCode2021\\Day_3_Input_Values.txt");
+string Line;
+
+
+while (getline(TestFileInput, Line)){
+    int LineLength = Line.length();
+
+
+    for(int i=0; i<LineLength; i++){
+        
+    }
+
+}
+
+
+
+
+return 0;
+}
