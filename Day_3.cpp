be7ffa77512fb317e99870f6c3baa209bdf7b67e
Finn McDonnell <fmcd743@UoA.auckland.ac.nz>
2022-01-17 23:45:15 +1300

    Finished Day 3 Part 1

--- Day_3.cpp
+++ Day_3.cpp
@@ -6,44 +6,33 @@ using namespace std;
 
 int main() {
 
-cout << "entering main" << endl;
-
 int LineCounter = 0;
-ifstream TestFileInput ("C:\\Users\\Finn McDonnell\\Documents\\VisualStudioCode\\AdventofCode2021\\Day_3_Input_Values");
+ifstream TestFileInput ("C:\\Users\\finnm\\OneDrive\\Documents\\Visual Studio\\AdventofCode2021\\Day_3_Input_Values.txt");
     if (!TestFileInput) {
-        cout << "Unable to open file";
         exit(1); // terminate with error
     }
 string Line;
 int Length = 0;
 vector<int> DiagBits = {};
 
-cout << "prewhile working" << endl;
-
 while (getline(TestFileInput, Line)){
     int LineLength = Line.length();
     if (LineLength>Length){
         DiagBits.resize(LineLength-Length);
     }
-
     for(int i=0; i<LineLength; i++){
-        if (Line[i]==1){
+        if (Line[i] & 1){
             DiagBits[i]++;
-            cout << "DiagBitsIncreasing" << endl;
         }
     }
     LineCounter++;
-    cout << "while working" << endl;
 }
-
-//std::vector<string> gamma(DiagBits.size(), "0");
-//std::vector<string> epsilon(DiagBits.size(), "0");
 
 string gamma;
 string epsilon;
 
 for (int i=0; i<DiagBits.size(); i++){
-    if (DiagBits[i]>((LineCounter+1)/2)){
+    if (DiagBits[i]>=((LineCounter+1)/2)){
         gamma.append("1");
         epsilon.append("0");
     } else {
@@ -52,11 +41,8 @@ for (int i=0; i<DiagBits.size(); i++){
     }
 }
 
-cout << gamma << endl;
-cout << epsilon << endl;
-cout << DiagBits.size() << endl;
-
-//int FinalValue = (stoi(gamma, 0, 2))*(stoi(epsilon, 0, 2));
+int FinalValue = (stoi(gamma, 0, 2))*(stoi(epsilon, 0, 2));
+std::cout << FinalValue << endl;
 
 return 0;
 }
